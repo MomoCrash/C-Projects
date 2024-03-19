ea07a118d6e4d3d4616b41537913f5ddad3382d0
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2024-03-19 09:55:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,15 @@
 
 typedef enum { false, true } bool;
 
+//#TODO naming convention
 typedef struct Tile
 {
 
-    bool IsMine;
-    bool IsShowed;
-    bool IsFlag;
+    bool isMine;
+    bool isShowed;
+    bool isFlag;
 
-    int MineNumberAround;
+    int mineNumberAround;
     int x;
     int y;
 
@@ -28,7 +29,7 @@ typedef struct Grid
     int size;
     Tile* tiles;
 
-    int DiscoveredTile;
+    int remainTiles; //remainingTile
 
 } Grid;
 
@@ -115,6 +116,14 @@ bool ContainInt(int* intArray, int value) {
 
 void Color(int couleurDuTexte, int couleurDeFond);
 
+Tile* GetTile( Grid* grid, int x, int y ) 
+{
+    // return NULL si en dheors de la range
+
+    //return & Tile
+
+}
+
 void MineArroundTile(Grid* grid, int x, int y) {
     Tile* t = grid->tiles + (grid->size * y + x);
     for (int yAR = y - 1; yAR <= y + 1; yAR++) {
@@ -124,8 +133,8 @@ void MineArroundTile(Grid* grid, int x, int y) {
 
             if (xAR >= grid->size || xAR < 0) continue;
             Tile arroundTile = *(grid->tiles + (grid->size * yAR + xAR));
-            if (arroundTile.IsMine) {
-                t->MineNumberAround++;
+            if (arroundTile.isMine) {
+                t->mineNumberAround++;
             }
 
         }
@@ -137,12 +146,12 @@ bool DiscoverTile(Grid* grid, int x, int y) {
     Tile* t = grid->tiles + (grid->size * y + x);
 
     if (t == NULL) return true;
-    if (t->IsShowed) return false;
+    if (t->isShowed) return false;
 
-    t->IsShowed = 1;
-    grid->DiscoveredTile++;
-    if (t->IsMine) return true;
-    if (t->MineNumberAround > 0) return false;
+    t->isShowed = 1;
+    grid->remainTiles++;
+    if (t->isMine) return true;
+    if (t->mineNumberAround > 0) return false;
 
     for (int yAR = y - 1; yAR <= y + 1; yAR++) {
         if (yAR >= grid->size || yAR < 0) continue;
@@ -150,7 +159,7 @@ bool DiscoverTile(Grid* grid, int x, int y) {
             if (xAR >= grid->size || xAR < 0) continue;
 
             Tile tile = *(grid->tiles + (grid->size * yAR + xAR));
-            if (tile.IsShowed) continue;
+            if (tile.isShowed) continue;
 
             DiscoverTile(grid, xAR, yAR);
         }
@@ -161,50 +170,50 @@ bool DiscoverTile(Grid* grid, int x, int y) {
 
 void PlaceFlag(Grid* grid, int x, int y) {
     Tile* t = grid->tiles + (grid->size * y + x);
-    if (t->IsFlag) {
-        t->IsFlag = false;
+    if (t->isFlag) {
+        t->isFlag = false;
     }
     else {
-        t->IsFlag = true;
+        t->isFlag = true;
     }
 }
 
-void PrintTile(Tile tile) {
-    if (tile.IsShowed && tile.IsMine) {
+void PrintTile(const Tile* tile) {
+    if (tile->isShowed && tile->isMine) {
         Color(12, 0);
         printf("| M |");
         Color(15, 0);
     }
-    else if (tile.IsShowed) {
-        switch (tile.MineNumberAround) {
+    else if (tile->isShowed) {
+        switch (tile->mineNumberAround) {
         case 0:
             Color(15, 0);
-            printf("| %d |", tile.MineNumberAround);
+            printf("| %d |", tile->mineNumberAround);
             Color(15, 0);
             break;
         case 1:
             Color(9, 0);
-            printf("| %d |", tile.MineNumberAround);
+            printf("| %d |", tile->mineNumberAround);
             Color(15, 0);
             break;
         case 2:
             Color(10, 0);
-            printf("| %d |", tile.MineNumberAround);
+            printf("| %d |", tile->mineNumberAround);
             Color(15, 0);
             break;
         case 3:
             Color(14, 0);
-            printf("| %d |", tile.MineNumberAround);
+            printf("| %d |", tile->mineNumberAround);
             Color(15, 0);
             break;
         default:
             Color(5, 0);
-            printf("| %d |", tile.MineNumberAround);
+            printf("| %d |", tile->mineNumberAround);
             Color(15, 0);
             break;
         }
     }
-    else if (tile.IsFlag) {
+    else if (tile->isFlag) {
         printf("| F |");
     }
     else {
@@ -213,20 +222,21 @@ void PrintTile(Tile tile) {
 }
 
 void InitTile(Tile* t, int x, int y) {
-    t->IsMine = false;
-    t->IsShowed = false;
-    t->IsFlag = false;
-    t->MineNumberAround = 0;
+    t->isMine = false;
+    t->isShowed = false;
+    t->isFlag = false;
+    t->mineNumberAround = 0;
     t->x;
     t->y;
 }
 
 // GRID
 
+//#TODO better algo
 void PlaceRandomMine(Grid* grid, int mineCount) {
 
     int* randomIntegers = (int*)malloc(sizeof(int) * mineCount);
-    if (!randomIntegers) return;
+    if (!randomIntegers) exit(1);
 
     for (int i = 0; i < mineCount; i++) {
         int randomValue = 0;
@@ -238,7 +248,7 @@ void PlaceRandomMine(Grid* grid, int mineCount) {
     }
 
     for (int i = 0; i < mineCount; i++)
-        (grid->tiles + *(randomIntegers + i))->IsMine = true;
+        (grid->tiles + *(randomIntegers + i))->isMine = true;
 
     free(randomIntegers);
 }
@@ -248,7 +258,7 @@ int CountGoodFlag(Grid* grid) {
     for (int y = 0; y < grid->size; y++) {
         for (int x = 0; x < grid->size; x++) {
             Tile* t = grid->tiles + (grid->size * y + x);
-            if (t->IsFlag && t->IsMine) {
+            if (t->isFlag && t->isMine) {
                 goodMine++;
             }
         }
@@ -256,8 +266,8 @@ int CountGoodFlag(Grid* grid) {
     return goodMine;
 }
 
-void PrintGrid(Grid* grid) {
-    //system("cls");
+void PrintGrid(const Grid* grid) {
+    system("cls");
     for (int y = 0; y < grid->size+1; y++) {
 
         if (y == 0) {
@@ -284,7 +294,7 @@ void PrintGrid(Grid* grid) {
             Color(15, 0);
         }
         for (int x = 0; x < grid->size; x++) {
-            PrintTile(*(grid->tiles + (grid->size * (y-1) + x)));
+            PrintTile((grid->tiles + (grid->size * (y-1) + x)));
         }
         printf("\n");
     }
@@ -293,7 +303,7 @@ void PrintGrid(Grid* grid) {
 void InitGrid(Grid* grid, int gridSize, int mineCount) {
     grid->tiles = (Tile*)malloc(sizeof(Tile) * gridSize * gridSize);
     grid->size = gridSize;
-    grid->DiscoveredTile = 0;
+    grid->remainTiles = 0;
 
     for (int y = 0; y < gridSize; y++) {
         for (int x = 0; x < gridSize; x++) {
@@ -314,12 +324,12 @@ void InitGrid(Grid* grid, int gridSize, int mineCount) {
 
 bool GameLoop(Grid* grid, int mineCount) {
 
-    bool Defeated = false;
-    bool Win = false; 
+    bool isDefeat = false;
+    bool isWin = false; 
     do {
 
-        bool Flag = AskChar("\n Mettre un drapeau ou decouvrir ?", "fFdD", "fF", "dD");
-        bool IsOnMine = false;
+        bool isFlag = AskChar("\n Mettre un drapeau ou decouvrir ?", "fFdD", "fF", "dD");
+        bool isOnMine = false;
 
         int x;
         int y;
@@ -330,29 +340,29 @@ bool GameLoop(Grid* grid, int mineCount) {
         x--;
         y--;
 
-        if (Flag) {
+        if (isFlag) {
 
             PlaceFlag(grid, x, y);
         }
         else {
 
-            IsOnMine = DiscoverTile(grid, x, y);
+            isOnMine = DiscoverTile(grid, x, y);
         }
 
         PrintGrid(grid);
 
-        if (IsOnMine) {
+        if (isOnMine) {
             printf("C'est une mine ! \n\n");
-            Defeated = true;
+            isDefeat = true;
         }
 
-        if (grid->DiscoveredTile == (grid->size * grid->size - (CountGoodFlag(grid) + (mineCount- CountGoodFlag(grid))))) {
+        if (grid->remainTiles == (grid->size * grid->size - (CountGoodFlag(grid) + (mineCount- CountGoodFlag(grid))))) {
             printf("Et c'est gagnee jeune entrepreneur !");
-            Win = true;
+            isWin = true;
         }
-    } while (!Defeated && !Win);
+    } while (!isDefeat && !isWin);
 
-    return (!Defeated && Win);
+    return (!isDefeat && isWin);
 }
 
 int main(void) {
@@ -363,9 +373,9 @@ int main(void) {
     int gridSize = 5;
     int bombNumber = 10;
 
-    bool WantReplay = true;
+    bool isReplaying = true;
 
-    while (WantReplay) {
+    while (isReplaying) {
 
         AskInt("Sur quel taille de plateau voulez-vous jouer ?", &gridSize, 0, 100);
         AskInt("Avec combiens de mine", &bombNumber, 0, gridSize*gridSize/2);
@@ -374,7 +384,7 @@ int main(void) {
 
         GameLoop(&grid, bombNumber);
 
-        WantReplay = AskChar("Voulez-vous rejouer ?", "yYnN", "yY", "nN");
+        isReplaying = AskChar("Voulez-vous rejouer ?", "yYnN", "yY", "nN");
 
     }
 
@@ -382,6 +392,7 @@ int main(void) {
 
 }
 
+// #TODO english , better name
 void Color(int couleurDuTexte, int couleurDeFond) // fonction d'affichage de couleurs
 {
     HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
